d2f452f28655e77a734ea100f16db14e1edd0356
Voy7 <89422331+Voy7@users.noreply.github.com>
2023-03-16 19:21:24 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react'
+
 // Home (index) page
 export default function Home() {
+  const [section, setSection] = useState(0)
+
   return (
     <div id="home">
       <h1>WEB GAME</h1>
       <main>
-        <button>Join Game</button>
-        <button>Create Game</button>
+        { section === 0 &&
+          <>
+            <button onClick={() => setSection(1)}>Join Game</button>
+            <button onClick={() => setSection(2)}>Create Game</button>
+          </>
+        }
+        { section == 1 &&
+          <>
+            <form>
+              <label htmlFor="gameId">Join Game</label>
+              <input type="text" id="gameId" />
+            </form>
+          </>
+        }
+        { section == 2 &&
+          <>
+            <h2>create game thing</h2>
+          </>
+        }
       </main>
     </div>
   )
